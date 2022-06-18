e66ea63ec4c8d09b0621f658e61eac8ad5a469c0
NicoleS1312 <102580592+nicole1312@users.noreply.github.com>
2022-06-18 10:27:23 +0100

    Created Weather Hub

---
+++ src/ForecastData.js
@@ -0,0 +1,172 @@
+import React, { useState } from "react";
+import axios from "axios";
+import ReactAnimatedWeatherUpdated from "react-animated-weather-updated";
+import "./App.css";
+
+export default function ForecastData() {
+  const [city, setCity] = useState(" ");
+  const [forecast, setForecast] = useState(" ");
+  const [loaded, setLoaded] = useState(false);
+
+  function showWeather(response) {
+    setLoaded(true);
+    setForecast({
+      date: "17 June 2022",
+      temperature: response.data.main.temp,
+      highTemperature: response.data.main.temp_max,
+      lowTemperature: response.data.main.temp_min,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      pressure: response.data.main.pressure,
+      icon: response.data.weather[0].icon,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "02743828b1081c945744035baeda7192";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+  function searchUpdate(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Type a city"
+        autoComplete="off"
+        onChange={searchUpdate}
+        className="form-control"
+      />
+      <button type="submit" className="btn ">
+        Search
+      </button>
+    </form>
+  );
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <div className="container">
+          <div className="weather">
+            <div className="row">
+              <div className="col-6">
+                <h4>{forecast.date}</h4>
+                <h3 className="city">{city}</h3>
+                <ul className="details">
+                  <li className="description">{forecast.description}</li>
+                  <li>
+                    <strong>{Math.round(forecast.temperature)}ºC</strong>
+                  </li>
+                  <div className="highLow">
+                    <li>
+                      <span>
+                        ⇧H:
+                        <strong>{Math.round(forecast.highTemperature)}º</strong>
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        ⇩L:
+                        <strong>{Math.round(forecast.lowTemperature)}º</strong>
+                      </span>
+                    </li>
+                  </div>
+                  <li>
+                    <img
+                      src={forecast.icon}
+                      alt={forecast.description}
+                      className="icon"
+                    />
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className=" analysis">
+              <div className="col-6 ">
+                <ul className="details">
+                  <li>
+                    <strong>Humidity:</strong>
+                    {forecast.humidity}%
+                  </li>
+                  <li>
+                    <strong>Wind:</strong>
+                    {Math.round(forecast.wind)}mph
+                  </li>
+                  <li>
+                    <strong>Pressure:</strong>
+                    {forecast.pressure}mb
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {form}
+        <div className="container">
+          <div className="weather">
+            <div className="row">
+              <div className="col-6">
+                <h4>18 June 2022</h4>
+                <h3 className="city">london</h3>
+                <ul className="details">
+                  <li className="description">clear sky</li>
+                  <li>
+                    <strong>21ºC</strong>
+                  </li>
+                  <div className="highLow">
+                    <li>
+                      <span>
+                        ⇧H:
+                        <strong>26º</strong>
+                      </span>
+                    </li>
+
+                    <li>
+                      <span>
+                        ⇩L:
+                        <strong>11º</strong>
+                      </span>
+                    </li>
+                  </div>
+                  <li className="icon">
+                    <ReactAnimatedWeatherUpdated
+                      icon="CLEAR_DAY"
+                      color="goldenrod"
+                      size={50}
+                      animate={true}
+                    />
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className=" analysis">
+              <div className="col-6 ">
+                <ul className="details">
+                  <li>
+                    <strong>Humidity:</strong> 70 %
+                  </li>
+                  <li>
+                    <strong>Wind: </strong>7 mph
+                  </li>
+                  <li>
+                    <strong>Pressure:</strong> 1019 mb
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import ForecastData from "./ForecastData";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box">
+        <h1>TheWeatherHub</h1>
+        <div>
+          <ForecastData />
+        </div>
+        <div></div>
+      </div>
+
+      <small>
+        <a
+          href="https://github.com/nicole1312/weather-hub-project"
+          target="_blank"
+          className="source"
+          rel="noopener noreferrer"
+        >
+          Open-source code
+        </a>
+        by Nicoleta Serban
+      </small>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
